bd1a3cb4acfdbf60ba5a06492c0cf4365efc71cb
Naresh Kumar <102852982+Naresh123github@users.noreply.github.com>
2023-08-28 11:10:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+var modal = document.getElementById('id01');
+                
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+                // create a age calcultor
+                var modal = document.getElementById('id02');
+                
+                // When the user clicks anywhere outside of the modal, close it
+                var calculate = document.getElementById('calculate'),
+                calculateAge = document.getElementById('calculateAge');
+              
+                calculate.addEventListener('click',function(){
+                  var birthDate = document.getElementById('birth_date').value;
+                  birthDate = new Date(birthDate);
+                  todayDate = new Date()
+                  totalAge = Date.now() - birthDate.getTime();
+                  ageYear = new Date(totalAge);
+                  ageYear = Math.abs(ageYear.getUTCFullYear() - 1970 );
+                  
+                  function ageMonth() {
+                    if(todayDate.getMonth() >= birthDate.getMonth()) {
+                        if(todayDate.getDate() >= birthDate.getDate()) {
+                            return todayDate.getMonth() - birthDate.getMonth();
+                        }
+                        else {
+                            if((todayDate.getMonth() - 1) >= birthDate.getMonth()) {
+                                return (todayDate.getMonth() - 1) - birthDate.getMonth();
+                            }
+                            else {
+                                return ((todayDate.getMonth() - 1) + 12) - birthDate.getMonth();
+                            }
+                        }
+                    }
+                      
+                  };
+                  function ageDay() {
+                      if(todayDate.getDate() > birthDate.getDate()) {
+                          return todayDate.getDate() - birthDate.getDate();
+                      }
+                      else if(todayDate.getDate() == birthDate.getDate()) {
+                          return todayDate.getDate() - birthDate.getDate();
+                      }
+                      else {
+                          let calDate = new Date(birthDate.getFullYear(), birthDate.getMonth(), 0);
+                          return (todayDate.getDate() + calDate.getDate()) - birthDate.getDate();
+                      }
+                  };
+                  calculateAge.innerHTML = "Your Age "+ageYear+" Years "+ageMonth()+ " Months "+ageDay()+" Days"
+              
+                });
